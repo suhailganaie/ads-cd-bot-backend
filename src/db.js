b617956442816fb7e43d42fceb698e6003a7266a
suhailganaie <63506421+suhailganaie@users.noreply.github.com>
2025-09-06 16:40:56 +0530

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import pg from 'pg';
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('render.com') ? { rejectUnauthorized: false } : false
+});
+
+export async function query(text, params) {
+  const res = await pool.query(text, params);
+  return res;
+}
